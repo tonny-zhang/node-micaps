9e99dd64c5c6343fd288f715288c2185ae6a3ee2
tonny <wodexintiao@gmail.com>
2015-02-11 19:01:55 +0800

    a

--- view/test/micaps_3/js/stroke.js
+++ view/test/micaps_3/js/stroke.js
@@ -5,9 +5,9 @@
 		, global_data_width //数据width
 		, global_data_height //数据height
 		, obj_flag = {} //存储标记后的结果，第个对象形如：{x: 2, y: 2, n: 2, n_u: 0}, n 为和自身颜色不一样的其它颜色个数, n_u 为已经参与了几个多边形的描边
-		,
-		arr_eara = [] //存储描完边的多边形，形如： {items: [{x: 110.11, y: 30.25}], color: '#ff0000', clip: [{x: 111.12, y: 23.2}]} , clip 是要对本多边形进行的挖空操作
-
+		, arr_eara = [] //存储描完边的多边形，形如： {items: [{x: 110.11, y: 30.25}], color: '#ff0000', clip: [{x: 111.12, y: 23.2}]} , clip 是要对本多边形进行的挖空操作
+		, obj_color_num = {}// 颜色个数
+		, arr_no_used = [];
 	function _get_around(x, y) {
 		var current_point = global_data[x][y]; // 得到当前点
 		var obj_return = {
@@ -58,45 +58,59 @@
 		return obj_flag[key];
 	}
 	/*得到点周围被标记的点数组*/
-	function _get_around_flag(x, y, is_return_next) {
+	function _get_around_flag(x, y, conf) {
+		var is_return_next = false,
+			is_get_lral = false;//是否保得到上下左右
+		if(conf){
+			is_return_next = conf.is_return_next;
+			is_get_lral = conf.is_get_lral;
+		}
 		var arr_return = [];
 		var obj_above = _get_flag(x, y - 1);
 		if (obj_above) {
 			arr_return.push(obj_above);
 		}
-		var obj_above_right = _get_flag(x + 1, y - 1);
-		if (obj_above_right) {
-			arr_return.push(obj_above_right);
+		if(!is_get_lral){
+			var obj_above_right = _get_flag(x + 1, y - 1);
+			if (obj_above_right) {
+				arr_return.push(obj_above_right);
+			}
 		}
 		var obj_right = _get_flag(x + 1, y);
 		if (obj_right) {
 			arr_return.push(obj_right);
 		}
-		var obj_below_right = _get_flag(x + 1, y + 1);
-		if (obj_below_right) {
-			arr_return.push(obj_below_right);
+		if(!is_get_lral){
+			var obj_below_right = _get_flag(x + 1, y + 1);
+			if (obj_below_right) {
+				arr_return.push(obj_below_right);
+			}
 		}
 		var obj_below = _get_flag(x, y + 1);
 		if (obj_below) {
 			arr_return.push(obj_below);
 		}
-		var obj_below_left = _get_flag(x - 1, y + 1);
-		if (obj_below_left) {
-			arr_return.push(obj_below_left);
+		if(!is_get_lral){
+			var obj_below_left = _get_flag(x - 1, y + 1);
+			if (obj_below_left) {
+				arr_return.push(obj_below_left);
+			}
 		}
 		var obj_left = _get_flag(x - 1, y);
 		if (obj_left) {
 			arr_return.push(obj_left);
 		}
-		var obj_above_left = _get_flag(x - 1, y - 1);
-		if (obj_above_left) {
-			arr_return.push(obj_above_left);
+		if(!is_get_lral){
+			var obj_above_left = _get_flag(x - 1, y - 1);
+			if (obj_above_left) {
+				arr_return.push(obj_above_left);
+			}
 		}
 		if (is_return_next) {
 			var color_current = _get_flag(x, y).color;
 			var obj_tmp;
 			while ((obj_tmp = arr_return.shift())) {
-				if (obj_tmp.color == color_current && obj_tmp.area.indexOf(int_global_area_index) == -1) {
+				if (obj_tmp.color == color_current) {// && obj_tmp.area.indexOf(int_global_area_index) == -1
 					return obj_tmp;
 				}
 			}
@@ -110,6 +124,7 @@
 			for (var j = 0; j < global_data_height; j++) {
 				var obj_info_around = _get_around(i, j);
 				if (obj_info_around.n > 0 || i == 0 || j == 0 || i == global_data_width - 1 || j == global_data_height - 1) {
+					var color_flag = (global_data[i][j]).color;
 					obj_flag[i + '_' + j] = {
 						x: i,
 						y: j,
@@ -117,19 +132,57 @@
 						n_u: 0,
 						around: obj_info_around,
 						area: [],
-						color: (global_data[i][j]).color
+						color: color_flag
 					};
+					if(!(color_flag in obj_color_num)){
+						obj_color_num[color_flag] = 0;
+					}
+					obj_color_num[color_flag]++;
 				}
 			}
 		}
+
+		for (var i in obj_flag) {
+			arr_no_used.push(obj_flag[i]);
+		}
+		arr_no_used.sort(function(a, b){
+			return obj_color_num[b.color] - obj_color_num[a.color];
+		});
+		//双边界处理成单边界
+		// for(var i in obj_flag){
+		// 	var obj_current_point = obj_flag[i];
+		// 	var int_x_current = obj_current_point.x,
+		// 		int_y_current = obj_current_point.y;
+
+		// 	if(int_x_current == 0 || int_y_current == 0 || int_x_current == global_data_width - 1 || int_y_current == global_data_height - 1){
+		// 		continue;
+		// 	}
+				
+		// 	var key_right = int_x_current+1+'_'+int_y_current;
+		// 	var obj_right_point = obj_flag[key_right];
+		// 	if(obj_right_point && obj_right_point.color != obj_current_point.color){
+		// 		delete obj_flag[obj_color_num[obj_right_point.color] > obj_color_num[obj_current_point.color]?i : key_right];
+		// 	}
+		// 	var key_below = int_x_current+'_'+(int_y_current+1);
+		// 	var obj_below_point = obj_flag[key_below];
+		// 	if(obj_below_point && obj_below_point.color != obj_current_point.color){
+		// 		delete obj_flag[obj_color_num[obj_below_point.color] > obj_color_num[obj_current_point.color]?i : key_below];
+		// 	}
+		// }
 	}
 	/*得到没有被使用的标记点*/
 	function _get_no_used_item() {
-		for (var i in obj_flag) {
-			var obj_item = obj_flag[i];
-			if (obj_item.n_u == 0) {
-				obj_item.n_u++;
-				return obj_item;
+		// for (var i in obj_flag) {
+		// 	var obj_item = obj_flag[i];
+		// 	if (obj_item.n_u == 0) {
+		// 		obj_item.n_u++;
+		// 		return obj_item;
+		// 	}
+		// }
+		var obj_return;
+		while((obj_return = arr_no_used.shift())){
+			if(obj_return.n_u == 0){
+				return obj_return;
 			}
 		}
 	}
@@ -145,11 +198,15 @@
 
 		var arr_replaced_flag = [],
 			int_fork_num = 0;
+		var flag_is_have_replace = false;
 		for (var i = 0, j = arr_around_flag.length; i < j; i++) {
 			var obj_item = arr_around_flag[i];
 			var int_n_u = obj_item.n_u;
 			if (int_n_u > 0 && obj_item.color != color_current_point) {
-				arr_replaced_flag.push(obj_item);
+				flag_is_have_replace = true;
+				if(obj_item.area.indexOf(int_global_area_index) == -1){
+					arr_replaced_flag.push(obj_item);
+				}
 			}
 			if (obj_last_next_point_global != obj_item && obj_item.color == color_current_point) {
 				int_fork_num++;
@@ -170,7 +227,7 @@
 			arr_replaced_flag[i].area.push(int_global_area_index);
 		}
 		obj_current_point.area.push(int_global_area_index);
-		if (arr_replaced_flag.length == 0) {
+		if (!flag_is_have_replace && arr_replaced_flag.length == 0) {
 			arr_replaced_flag.push(obj_current_point);
 		}
 
@@ -180,7 +237,11 @@
 	function _next_is_first(x, y, obj_first_items) {
 		var arr_arround = _get_around_flag(x, y);
 		for (var i = 0, j = obj_first_items.length; i < j; i++) {
-			if (arr_arround.indexOf(obj_first_items[i]) > -1) {
+			// if (arr_arround.indexOf(obj_first_items[i]) > -1) {
+			// 	return true;
+			// }
+			var item = obj_first_items[i];
+			if(item.x == x && item.y == y){
 				return true;
 			}
 		}
@@ -189,31 +250,27 @@
 	function _get_next_put_items(x, y, obj_first_items) {
 		var obj_current_point = _get_flag(x, y); // 得到当前点
 		var color_current_point = obj_current_point.color;
-		var obj_next = _get_around_flag(x, y, true);
-
+		var obj_next;
+		var int_current_x = x, int_current_y = y;
 		var arr_return_items = [];
-		if (obj_next) {
-			console.log(1, obj_next);
-			var arr_next_items;
-			while ((arr_next_items = _replace_current(obj_next.x, obj_next.y)) && arr_next_items.length > 0) {
-				arr_return_items = arr_return_items.concat(arr_next_items);
-				obj_next = _get_around_flag(obj_next.x, obj_next.y, true);
-				if (obj_next) {
-					console.log(2, obj_next);
-					arr_return_items = arr_return_items.concat(_replace_current(obj_next.x, obj_next.y));
-					var arr_get = _get_next_put_items(obj_next.x, obj_next.y, obj_first_items);
-					if (arr_get.length == 0) {
-						break;
-					} else {
-						arr_return_items = arr_return_items.concat(arr_get);
-					}
-				} else {
-					break;
-				}
-
-			}
-			return arr_return_items;
+		while((obj_next = _get_around_flag(int_current_x, int_current_y, {
+			is_return_next: true
+		}))){
+			obj_next.n_u++; //增加使用记录
+			int_current_x = obj_next.x;
+			int_current_y = obj_next.y;
+			var arr_next_items = _replace_current(int_current_x, int_current_y);
+			arr_return_items = arr_return_items.concat(arr_next_items);
 		}
+		if(_next_is_first(x, y, obj_first_items)){//填充最后一个点
+			arr_return_items = arr_return_items.concat(obj_first_items);
+		}else{
+
+		}
+		return arr_return_items;
+	}
+	function _reback(){
+
 	}
 	var int_global_area_index = 0;
 	/*把已经标记的点进行描边操作*/
