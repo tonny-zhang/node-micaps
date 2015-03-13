3e50f2a812a437e94da2ee8ceeb8d35cd4fd689d
tonny <wodexintiao@gmail.com>
2015-03-13 10:29:29 +0800

    修复option传递问题

--- shell/libs/types/1.js
+++ shell/libs/types/1.js
@@ -1,6 +1,7 @@
 exports.parse = function(lines, options){
 	options || (options = {});
 	options.num_of_cols = 24;
-	options.val_col = 7;
+	// options.val_col = 7;
+	options.default_val = 9999;
 	return require('./discrete').parse(lines, options);
 }
--- shell/libs/types/8.js
+++ shell/libs/types/8.js
@@ -1,6 +1,6 @@
 exports.parse = function(lines, options){
 	options || (options = {});
 	options.num_of_cols = 12;
-	options.val_col = 7;
+	// options.val_col = 7;
 	return require('./discrete').parse(lines, options);
 }
